b4fd666b30671f12fd14b51b45c8df21ecf451b4
JIANG mingda <jiangrivers9511@gmail.com>
2020-01-20 14:24:33 +0800

    Update Getting_start_with_images.py

--- Getting_start_with_images.py
+++ Getting_start_with_images.py
@@ -33,7 +33,7 @@ import cv2
 img = cv2.imread('./thor.jpg',0)
 cv2.imshow('image',img)
 k = cv2.waitKey(0) & 0xFF
-if k == 27:         # wait for ESC key to exit
+if k==27:         # wait for ESC key to exit
     cv2.destroyAllWindows()
-elif k == ord('s'): # wait for 's' key to save and exit
+elif k==ord('s'): # wait for 's' key to save and exit
     cv2.destroyAllWindows()
