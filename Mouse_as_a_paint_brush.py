81030911aaa8cd3e8d4dfbdc85ad5951ed2a6488
JiangRIVERS <jiangrivers9511@gmail.com>
2020-02-18 19:44:10 +0800

    Update Mouse_as_a_paint_brush.py

--- Mouse_as_a_paint_brush.py
+++ Mouse_as_a_paint_brush.py
@@ -20,26 +20,26 @@ import cv2
 # mouse callback function
 def draw_circle(event,x,y,flags,param):
     # MouseEventFlags ：
-        #   cv.EVENT_FLAG_LBUTTON = 1, 左键拖拽
-        #   cv.EVENT_FLAG_RBUTTON = 2, 右键拖拽
-        #   cv.EVENT_FLAG_MBUTTON = 4, 中键不放
-        #   cv.EVENT_FLAG_CTRLKEY = 8,按住ctrl不放
-        #   cv.EVENT_FLAG_SHIFTKEY = 16, 按住shift不放
-        #   cv.EVENT_FLAG_ALTKEY = 32 ,按住alt不放
+        #   cv2.EVENT_FLAG_LBUTTON = 1, 左键拖拽
+        #   cv2.EVENT_FLAG_RBUTTON = 2, 右键拖拽
+        #   cv2.EVENT_FLAG_MBUTTON = 4, 中键不放
+        #   cv2.EVENT_FLAG_CTRLKEY = 8,按住ctrl不放
+        #   cv2.EVENT_FLAG_SHIFTKEY = 16, 按住shift不放
+        #   cv2.EVENT_FLAG_ALTKEY = 32 ,按住alt不放
 
-    #  cv.MouseEventTypes ：
-        #   cv.EVENT_MOUSEMOVE = 0, 鼠标移动
-        #   cv.EVENT_LBUTTONDOWN = 1, 左键按下
-        #   cv.EVENT_RBUTTONDOWN = 2, 右键按下
-        #   cv.EVENT_MBUTTONDOWN = 3, 中键按下
-        #   cv.EVENT_LBUTTONUP = 4, 左键释放
-        #   cv.EVENT_RBUTTONUP = 5, 右键释放
-        #   cv.EVENT_MBUTTONUP = 6, 中键释放
-        #   cv.EVENT_LBUTTONDBLCLK = 7, 左键双击
-        #   cv.EVENT_RBUTTONDBLCLK = 8, 右键双击
-        #   cv.EVENT_MBUTTONDBLCLK = 9, 中健双击
-        #   cv.EVENT_MOUSEWHEEL = 10, 滚轮滑动
-        #   cv.EVENT_MOUSEHWHEEL = 11 横向滚轮滑动
+    #  cv2.MouseEventTypes ：
+        #   cv2.EVENT_MOUSEMOVE = 0, 鼠标移动
+        #   cv2.EVENT_LBUTTONDOWN = 1, 左键按下
+        #   cv2.EVENT_RBUTTONDOWN = 2, 右键按下
+        #   cv2.EVENT_MBUTTONDOWN = 3, 中键按下
+        #   cv2.EVENT_LBUTTONUP = 4, 左键释放
+        #   cv2.EVENT_RBUTTONUP = 5, 右键释放
+        #   cv2.EVENT_MBUTTONUP = 6, 中键释放
+        #   cv2.EVENT_LBUTTONDBLCLK = 7, 左键双击
+        #   cv2.EVENT_RBUTTONDBLCLK = 8, 右键双击
+        #   cv2.EVENT_MBUTTONDBLCLK = 9, 中健双击
+        #   cv2.EVENT_MOUSEWHEEL = 10, 滚轮滑动
+        #   cv2.EVENT_MOUSEHWHEEL = 11 横向滚轮滑动
     if event==cv2.EVENT_LBUTTONDBLCLK:
         cv2.circle(img,(x,y),100,(255,0,0),-1)
 
